668bebe9e6db29fccb434add30150ab1818c8cee
Leo Ruiz <ruizruizleo@hotmail.com>
2021-05-15 09:52:58 -0300

    print f

--- Exercicios/Laços/for/tabuada.py
+++ Exercicios/Laços/for/tabuada.py
@@ -1,5 +1,5 @@
 num = int(input("digite um numero inteiro "))                 #solicita um numero ao usuario
 
 for x in range(1,11):                                         #define um laço iniciando em 1 ate 11
-    print(str(x)+" * "+str(num)+" = ", end='')                #mostra na tela x * numero escolhido = resultado
+    print(f"{x} * {num} = ", end='')                 #mostra na tela x * numero escolhido = resultado
     print(num*x)
