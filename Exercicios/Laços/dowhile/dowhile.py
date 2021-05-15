7a0a0ba8524a8ce1203e7ca4e903d2943bf6f154
Leo Ruiz <ruizruizleo@hotmail.com>
2021-05-15 09:54:16 -0300

    Update dowhile.py

--- Exercicios/Laços/dowhile/dowhile.py
+++ Exercicios/Laços/dowhile/dowhile.py
@@ -3,5 +3,5 @@ i = 1                 #variavel contador
 while True:           #while iniciando como verdadeiro
     print(i)          #mostra na tela o valor de i
     i = i + 1         #faz a iteraçao na variavel i
-    if(i > 10):       #testa se i é maior que 10
+    if i > 10 :       #testa se i é maior que 10
         break         #caso verdadeiro quebra o laço
