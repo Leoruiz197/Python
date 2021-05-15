d879a26d4ac800c5e1275fc947fc1c15d3953889
Leo Ruiz <ruizruizleo@hotmail.com>
2021-05-14 22:42:39 -0300

    Exercicios de Laço

---
+++ Python - Exercicios/Laços/for/forElse.py
@@ -0,0 +1,4 @@
+for x in range(10):                         #inicio do loop
+    print(x)                                #mostrar o valor de x na tela
+else:
+    print("final do laço")                  #ao acabar o laço mostrara na tela a frase
---
+++ Python - Exercicios/Laços/while/while.py
@@ -0,0 +1,5 @@
+i = 1                          #variavel contador
+
+while i < 10:                  #inicio do laço passando a condiçao 
+  print(i)                     #mostrando na tela o valor de i
+  i += 1                       #iterando no contador
